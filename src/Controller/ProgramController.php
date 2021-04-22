3fcbaf4a91114ce8ce3fdbae6500c51f6f6123d0
lolaonthemoon <laurence.idiart@gmai.com>
2021-04-22 22:27:29 +0200

    formulaires dans symfony pour category et program

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -13,13 +13,51 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramType;
+
+// Don't forget the Request use for the new method
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/programs", name="program_")
  */
+
 class ProgramController extends AbstractController
 
 {
+        /**
+     * The controller for the program add form or deal with
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request): Response
+    {
+        // Create a new Program Object
+        $program = new Program();
+        // Create the associated Form
+        $form = $this->createForm(ProgramType::class, $program);
+
+        // Get data from HTTP request
+        $form->handleRequest($request);
+
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Category Object
+            $entityManager->persist($program);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('program_index');
+        }
+
+        // Render the form
+        return $this->render('program/new.html.twig', ["form" => $form->createView()]);
+    }
+    // Attention, la methode show doit etre plus bas
+    //  pour eviter la confusion des routes
 
 
     /**
@@ -42,22 +80,17 @@ class ProgramController extends AbstractController
 
 
     /**
-     * Getting a program by id
-     *
-     * @Route("/show/{id<^[0-9]+$>}", name="show")
+     * Getting a program using param converter on program
+     * @Route("/show/{program}", name="show")
      * @return Response
      */
-    public function show(int $id): Response
+    public function show(Program $program): Response
     {
-        $program = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findOneBy(['id' => $id]);
-
-
+   
 
         if (!$program) {
             throw $this->createNotFoundException(
-                'No program with id : ' . $id . ' found in program\'s table.'
+                'No program found in program\'s table.'
             );
         }
 
@@ -77,38 +110,46 @@ class ProgramController extends AbstractController
     /**
      * Getting details for a season by season id and program id
      *
-     * @Route("/{programId<^[0-9]+$>}/seasons/{seasonId<^[0-9]+$>}", name="season_show")
+     * @Route("/{program}/seasons/{season}", name="season_show")
      * @return Response
      */
-    public function showSeason(int $programId, int $seasonId): Response
+    public function showSeason(Program $program , Season $season): Response
     {
-        $program = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findOneBy(['id' => $programId]);
-
-        $season = $this->getDoctrine()
-            ->getRepository(Season::class)
-            ->findOneBy(['id' => $seasonId]);
-
-        // code pas correct car l' id  n'est pas le bon
-        $episodes = $this->getDoctrine()
-            ->getRepository(Season::class)
-            ->findBy(['id' => $seasonId]);
-
-
         if (!$program or !$season) {
             throw $this->createNotFoundException(
                 'Problème, Problème,.. avec un P majuscule'
             );
-        } elseif (!$episodes) {
-            throw $this->createNotFoundException(
-                'Pas d episode'
-            );
         } else {
             return $this->render('season/show.html.twig', [
-                'program' => $program, 'season' => $season, 'episodes' => $episodes,
+                'program' => $program, 'season' => $season
             ]);
-        } {
         };
     }
+
+
+        /**
+     * Getting program with seasons with episodes
+     *
+     * @Route("/{program}/seasons/{season}/episodes/{episode}", name="episode_show")
+     * 
+     * @return Response
+     */
+    public function showEpisode(Program $program, Season $season, Episode $episode): Response
+    {
+        if (!$program or !$season or !$episode) {
+            throw $this->createNotFoundException(
+                'Problème, Problème,.. avec un P majuscule'
+            );
+        } else {
+            return $this->render('program/episode_show.html.twig', [
+                'program' => $program, 'season' => $season, 'episode'=> $episode
+            ]);
+        };
+    }
+
+
+
+
+
+
 }
